20a14075d280cfa14d4bb0b213809f3b0c18816d
JN-GP <jowynikolaygamboapenuela@soy.sena.edu.co>
2026-07-15 21:50:58 -0500

    v0.1_clasesPaqueteModelCompletas_JN-GP_15072023

--- src/main/java/com/sena/parqueadero/model/Tarifa.java
+++ src/main/java/com/sena/parqueadero/model/Tarifa.java
@@ -1,5 +1,36 @@
 package com.sena.parqueadero.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "tarifas")
 public class Tarifa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idTarifa;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TipoVehiculo tipoVehiculo;
+	@Column(nullable = false)
+	private Double tarifaPorHora;
+	@Column(nullable = false)
+	private Double tarifaPorDia;
 }
--- src/main/java/com/sena/parqueadero/model/Vehiculo.java
+++ src/main/java/com/sena/parqueadero/model/Vehiculo.java
@@ -1,5 +1,36 @@
 package com.sena.parqueadero.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "vehiculos")
 public class Vehiculo {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idVehiculo;
+	@Column(unique = true, nullable = false)
+	private String placa;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TipoVehiculo tipo;
+	private String marca;
+	private String modelo;
 }
